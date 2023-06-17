1bc0c4f545d9acd0c9eae53402dc9c80d78dfc08
guidoRosito98 <guidorvp@hotmail.com>
2023-06-17 14:21:29 -0300

    Adding MaterialUI and Axios to the proyect

---
+++ app/src/app/sellers/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Typography } from "../common/components/materialUI";
+
+const SellersPage = () => {
+
+  return (
+    <Typography>Vendedores</Typography>
+  )
+}
+
+export default SellersPage;
