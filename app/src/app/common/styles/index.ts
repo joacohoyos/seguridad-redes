5542f21e947a5f7c0dc7e11765f96289d075fa95
guidoRosito98 <guidorvp@hotmail.com>
2023-06-17 17:15:19 -0300

    Login Screen / UI Setup

---
+++ app/src/app/common/styles/index.ts
@@ -0,0 +1,24 @@
+export const layoutBoxStyle = {
+  backgroundColor: "#8229FF",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  width: "100%",
+  height: "100vh",
+};
+
+export const fullWidthBoxStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+};
+
+export const themedButtonStyle = {
+  width: "40%",
+  marginY: 2,
+  backgroundColor: "#8229FF",
+  "&:hover": {
+    backgroundColor: "#6b0afc",
+  },
+};
---
+++ app/src/app/login/styles.ts
@@ -0,0 +1,11 @@
+export const loginBox = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  marginBottom: 10,
+  width: 400,
+  height: 300,
+  borderRadius: 4,
+  padding: 3,
+  backgroundColor: "white",
+};
