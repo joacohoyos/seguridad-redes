633d94644052ba286d50e48ea16116d7232cd3b3
iunter <ivounter@gmail.com>
2023-07-01 19:50:33 -0300

    Added source show

--- app/src/app/common/confirmPasswordChange.ts
+++ app/src/app/common/confirmPasswordChange.ts
@@ -3,7 +3,7 @@ import { ENDPOINT_USERS } from "@/app/common/routes";
 
 export const confirmPasswordChange = async (email: string) => {
   try {
-    const putRes = api.put(ENDPOINT_USERS, {});
+    const putRes = api.post(ENDPOINT_USERS, {});
   } catch (e) {
     console.log(e);
   }
--- app/src/app/common/routes/index.ts
+++ app/src/app/common/routes/index.ts
@@ -5,5 +5,8 @@ export const ENDPOINT_USERS = "/users";
 export const endpointPutProductName = (product_id: string) =>
   `${ENDPOINT_PRODUCTS}/${product_id}/name`;
 
-  export const endpointChangePassword = (user_email: string) =>
+export const endpointChangePassword = (user_email: string) =>
   `${ENDPOINT_USERS}/${user_email}`;
+
+export const endpointConfirmPasswordChange = (user_email: string) =>
+`${ENDPOINT_USERS}/${user_email}/password`;
